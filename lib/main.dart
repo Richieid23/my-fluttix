e42bb1cb0ce5699c4365a254dc1ba62b8981f7ec
Richieid23 <fitrarizki23@gmail.com>
2022-02-08 18:55:36 +0700

    add signUp test button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_fluttix/services/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,9 +8,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                  child: Text('Sign Up'),
+                  onPressed: () async {
+                    SignInSignUpResult result = await AuthServices.signUp(
+                        'lisa@blackpink.com',
+                        '12345678',
+                        'Lalisa',
+                        ['Action', 'Horror', 'Music', 'Drama'],
+                        'Korean');
+
+                    if (result.user == null) {
+                      print(result.message);
+                    } else {
+                      print(result.user.toString());
+                    }
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
