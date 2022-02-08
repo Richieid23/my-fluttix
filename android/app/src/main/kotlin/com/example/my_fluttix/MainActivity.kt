2865947bdd9a231eb1cbd4d37ea9191d27236cbf
Richieid23 <fitrarizki23@gmail.com>
2022-02-08 09:39:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/my_fluttix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_fluttix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/services.dart
@@ -0,0 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_fluttix/models/models.dart';
+import 'package:my_fluttix/extensions/extensions.dart';
+
+part 'auth_services.dart';
