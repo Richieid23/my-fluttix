5e9b099ac56578c99d96200f1fd3305c61c35167
Richieid23 <fitrarizki23@gmail.com>
2022-02-08 18:52:11 +0700

    add cloud firestore import package

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_fluttix/models/models.dart';
 import 'package:my_fluttix/extensions/extensions.dart';
 
 part 'auth_services.dart';
+part 'user_sevices.dart';
