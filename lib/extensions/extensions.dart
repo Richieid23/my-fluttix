83a16735c058095ff0ef4f1ed5dec50a12c9c5ee
Richieid23 <fitrarizki23@gmail.com>
2022-02-08 20:19:20 +0700

    add import services

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_fluttix/models/models.dart';
+import 'package:my_fluttix/services/services.dart';
 
 part 'firebase_user_extension.dart';
