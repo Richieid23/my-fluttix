203a9848d4b355cb7ac162467b7898fc8399ad46
Richieid23 <fitrarizki23@gmail.com>
2022-02-10 10:15:27 +0700

    add sign in page ui

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,5 +1,8 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:my_fluttix/bloc/blocs.dart';
 import 'package:my_fluttix/services/services.dart';
 import 'package:my_fluttix/shared/shared.dart';
--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -8,18 +8,165 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isEmailValid = false;
+  bool isPasswordValid = false;
+  bool isSigningIn = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign In Page'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Sign In'),
-          onPressed: () {
-            AuthServices.signIn('lisa@blackpink.com', '12345678');
-          },
+    context
+        .bloc<ThemeBloc>()
+        .add(ChangeTheme(ThemeData().copyWith(primaryColor: accentColor2)));
+
+    return WillPopScope(
+      onWillPop: () {
+        context.bloc<PageBloc>().add(GoToSplashPage());
+        return;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: defaultMargin),
+          child: ListView(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  SizedBox(
+                    height: 70,
+                    child: Image.asset('assets/logo.png'),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 70, bottom: 40),
+                    child: Text(
+                      'Welcome back, \nExplorer!',
+                      style: blackTextFont.copyWith(fontSize: 20),
+                    ),
+                  ),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      labelText: 'Email Address',
+                      hintText: 'Email Address',
+                    ),
+                    onChanged: (text) {
+                      setState(() {
+                        isEmailValid = EmailValidator.validate(text);
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      labelText: 'Password',
+                      hintText: 'Password',
+                    ),
+                    onChanged: (text) {
+                      setState(() {
+                        isPasswordValid = text.length >= 6;
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Forgot Password?',
+                        style: greyTextFont.copyWith(
+                            fontSize: 12, fontWeight: FontWeight.w400),
+                      ),
+                      Text(
+                        'Get Now',
+                        style: purpleTextFont.copyWith(fontSize: 12),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      margin: const EdgeInsets.only(top: 40, bottom: 30),
+                      child: isSigningIn
+                          ? SpinKitFadingCircle(
+                              color: mainColor,
+                            )
+                          : FloatingActionButton(
+                              elevation: 0,
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: (isEmailValid && isPasswordValid)
+                                    ? Colors.white
+                                    : const Color(0xffbebebe),
+                              ),
+                              backgroundColor: (isEmailValid && isPasswordValid)
+                                  ? mainColor
+                                  : const Color(0xFFE4E4E4),
+                              onPressed: (isEmailValid && isPasswordValid)
+                                  ? () async {
+                                      setState(() {
+                                        isSigningIn = true;
+                                      });
+
+                                      SignInSignUpResult result =
+                                          await AuthServices.signIn(
+                                              emailController.text,
+                                              passwordController.text);
+
+                                      if (result.user == null) {
+                                        setState(() {
+                                          isSigningIn = false;
+                                        });
+
+                                        Flushbar(
+                                          duration: const Duration(seconds: 4),
+                                          flushbarPosition:
+                                              FlushbarPosition.TOP,
+                                          backgroundColor:
+                                              const Color(0xffff5c83),
+                                          message: result.message,
+                                        ).show(context);
+                                      }
+                                    }
+                                  : null,
+                            ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Start Fresh Now?',
+                        style: greyTextFont.copyWith(
+                            fontSize: 12, fontWeight: FontWeight.w400),
+                      ),
+                      Text(
+                        'Sign Up',
+                        style: purpleTextFont.copyWith(fontSize: 12),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
