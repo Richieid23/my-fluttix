4f5d949962d6e459f94b5ef045a028f3a0315edc
Richieid23 <fitrarizki23@gmail.com>
2022-03-02 18:50:06 +0700

    add credit & date card part

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:my_fluttix/models/models.dart';
 import 'package:my_fluttix/shared/shared.dart';
+import 'package:my_fluttix/extensions/extensions.dart';
 
 part 'selectable_box.dart';
 part 'movie_card.dart';
@@ -9,3 +10,5 @@ part 'rating_stars.dart';
 part 'browse_button.dart';
 part 'coming_soon_card.dart';
 part 'promo_card.dart';
+part 'credit_card.dart';
+part 'date_card.dart';
