32b9a707a94f25351246bd499e839041430fcedd
Richieid23 <fitrarizki23@gmail.com>
2022-03-04 06:45:52 +0700

    add select seat page

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -16,6 +16,7 @@ import 'package:my_fluttix/shared/shared.dart';
 import 'package:my_fluttix/ui/widgets/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:random_string/random_string.dart';
 
 part 'splash_page.dart';
 part 'wrapper.dart';
@@ -27,3 +28,4 @@ part 'preference_page.dart';
 part 'account_confirmation_page.dart';
 part 'movie_detail_page.dart';
 part 'select_schedule_page.dart';
+part 'select_seat_page.dart';
