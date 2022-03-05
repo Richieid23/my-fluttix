754fade520528c12b7fb0934cc8580aef5720722
Richieid23 <fitrarizki23@gmail.com>
2022-03-05 18:27:13 +0700

    add ticket detail page

---
+++ lib/ui/pages/ticket_detail_page.dart
@@ -0,0 +1,11 @@
+part of 'pages.dart';
+
+class TicketDetailPage extends StatelessWidget {
+  final Ticket ticket;
+  const TicketDetailPage({Key key, @required this.ticket}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
